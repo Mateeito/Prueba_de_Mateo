0435fe74d748fc3b52dea2306bee79dd069701c8
Your Name <you@example.com>
2025-03-12 12:04:52 -0300

    agrego 2 funciones

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,11 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+
+def fucion():
+    print("warwick jg esta sobrevalorado")
+    num_1 = 4
+    num_2 = num_1*2
+    print(num_2)
+    
+
